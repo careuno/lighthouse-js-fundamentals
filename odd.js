72f44afe586f386ce43f99526496c46b21214761
Karen <to.karenngo@gmail.com>
2021-11-05 19:52:43 -0400

    Add file, isOdd function

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(num){
+ return !(num % 2 === 0);
+}
+
+console.log(isOdd(13))
+
+console.log("3 is odd: " + isOdd(0));
+console.log("8 is odd: " + isOdd(1));
+
+// input edge cases when testing, 0, 1 etc. 
